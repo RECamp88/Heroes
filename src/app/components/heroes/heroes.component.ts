5210b483046303c6135ca400708fb3186eb36acc
RECamp88 <recamp88@gmail.com>
2023-08-16 15:19:54 -0500

    worked on completing the tutorial the in-memory-data is not pulling correctly.

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,32 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Hero } from 'src/app/models/hero';
 import { HeroService } from 'src/app/services/hero.service';
-import { MessageService } from 'src/app/services/message.service';
+
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
-  selectedHero?: Hero; 
+  constructor(private heroService: HeroService) { }
 
-  constructor(private heroService : HeroService, private messageService : MessageService) {}
-
-  ngOnIgnit(): void {
+  ngOnInit(): void {
     this.getHeroes();
   }
 
   getHeroes(): void {
     this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
-  } 
+    .subscribe(heroes => this.heroes = heroes);
+  }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero=hero;
-    this.messageService.add('HeroesComponent: Selected hero id=${hero.id}');
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
   }
 }
