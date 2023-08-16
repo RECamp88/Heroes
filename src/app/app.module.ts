0adb4ef8f4cc75c849aa7644b2e1554c2978a4b6
RECamp88 <recamp88@gmail.com>
2023-08-16 13:07:21 -0500

    Added in services and messages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
+import { MessagesComponent } from './components/messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+
 import { Hero } from 'src/app/models/hero';
-import { HEROES } from 'src/app/models/mock-heros';
+import { HeroService } from 'src/app/services/hero.service';
+import { MessageService } from 'src/app/services/message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,15 +10,23 @@ import { HEROES } from 'src/app/models/mock-heros';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = [];
 
-  hero: Hero ={
-    id: 0,
-    name: ''
+  selectedHero?: Hero; 
+
+  constructor(private heroService : HeroService, private messageService : MessageService) {}
+
+  ngOnIgnit(): void {
+    this.getHeroes();
   }
 
-  selectedHero?: Hero;
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  } 
+
   onSelect(hero: Hero): void {
     this.selectedHero=hero;
+    this.messageService.add('HeroesComponent: Selected hero id=${hero.id}');
   }
 }
